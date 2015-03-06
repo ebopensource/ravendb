c0b43d83feb195f2553226952154ffee0eaa6dbe
Rob Scott <rob@eventbooking.com>
2015-03-06 13:22:40 -0500

    Unit test demonstrating deserialization failure

---
+++ Raven.Tests.Issues/EventBookingTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Raven.Imports.Newtonsoft.Json;
+using Raven.Tests.Common;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+	public class EventBookingTests : RavenTest
+	{
+		[Fact]
+		public void Read_only_property_is_accessed_during_deserialization()
+		{
+			var serializer = new JsonSerializer { ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor };
+
+			var stringWriter = new StringWriter();
+			serializer.Serialize( new JsonTextWriter( stringWriter ), new DomainClass { IntVal = 1 } );
+
+			var json = stringWriter.ToString();
+
+			serializer.Deserialize<DomainClass>( new JsonTextReader( new StringReader( json ) ) );
+		}
+
+		private class DomainClass
+		{
+			// We have a read-only property that relies on a writable property.
+			// This property should be serialized; but should not be referenced 
+			// during the deserialization process. Because the object is in an 
+			// invalid state, 
+			public ValueClass ReadOnlyVal
+			{
+				get { return 100 / IntVal > 0 ? new ValueClass() : null; }
+			}
+
+			public int IntVal { get; set; }
+		}
+
+		private class ValueClass
+		{
+		}
+	}
+}
